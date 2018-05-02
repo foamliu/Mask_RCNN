d7e2d988bf66decf222000f2988550c660174069
Yang Liu <foamliu@yeah.net>
2018-05-02 14:24:00 +0800

    add proc video

---
+++ samples/proc_video.py
@@ -0,0 +1,92 @@
+import os
+import cv2 as cv
+import time
+
+import coco
+import utils
+import model as modellib
+
+
+class App:
+    def __init__(self):
+        # Root directory of the project
+        ROOT_DIR = os.getcwd()
+
+        # Directory to save logs and trained model
+        MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+        # Local path to trained weights file
+        Mask_RCNN_ROOT_DIR = '.'
+        COCO_MODEL_PATH = os.path.join(Mask_RCNN_ROOT_DIR, "mask_rcnn_coco.h5")
+        # Download COCO trained weights from Releases if needed
+        if not os.path.exists(COCO_MODEL_PATH):
+            utils.download_trained_weights(COCO_MODEL_PATH)
+
+        class InferenceConfig(coco.CocoConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+
+        config = InferenceConfig()
+        config.display()
+
+        # Create model object in inference mode.
+        self.model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+        # Load weights trained on MS-COCO
+        self.model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+        # COCO Class names
+        # Index of the class in the list is its ID. For example, to get ID of
+        # the teddy bear class, use: class_names.index('teddy bear')
+        self.class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+                            'bus', 'train', 'truck', 'boat', 'traffic light',
+                            'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+                            'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+                            'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+                            'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+                            'kite', 'baseball bat', 'baseball glove', 'skateboard',
+                            'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+                            'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+                            'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+                            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+                            'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+                            'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+                            'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+                            'teddy bear', 'hair drier', 'toothbrush']
+
+    def run(self):
+        cap = cv.VideoCapture('joined_files.ts')
+
+        frame_idx = 0
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print('ret is False')
+                break
+
+            results = self.model.detect([frame], verbose=0)
+            #r = results[0]
+            app_results.append(results[0])
+
+            frame_idx = frame_idx + 1
+            print(frame_idx)
+
+        cap.release()
+        #out.release()
+        #cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    start = time.time()
+    import pickle
+    app_results = []
+    App().run()
+    output = open('data.pkl', 'wb')
+    # Pickle dictionary using protocol 0.
+    pickle.dump(app_results, output)
+    output.close()
+    end = time.time()
+    print('{} seconds'.format(end - start))
